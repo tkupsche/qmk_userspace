f0275b375fe834d3bebaa3dcaea6c864a4674443
Travis Kupsche <tkupsche@gmail.com>
2025-03-01 11:21:36 -0800

    First working commit

---
+++ keyboards/crkbd/keymaps/tk/config.h
@@ -0,0 +1,44 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+// https://docs.qmk.fm/features/rgb_matrix#custom-rgb-matrix-effects
+//#define USE_MATRIX_I2C
+
+#define TAPPING_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 2
+
+// compile size reduction
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_SLEEP // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+// #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash. 
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#endif
---
+++ keyboards/crkbd/keymaps/tk/keymap.c
@@ -0,0 +1,134 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define SFT_S LSFT_T(KC_S)
+#define ALT_D LALT_T(KC_D)
+#define GUI_F LGUI_T(KC_F)
+
+
+// Right-hand home row mods
+#define GUI_J RALT_T(KC_J)
+#define ALT_K RALT_T(KC_K)
+#define SFT_L RSFT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
+#define KC_TIK KC_GRV
+#define KC_TLD S(KC_GRV)
+
+// COMBOS
+#define COMBO_ONLY_FROM_LAYER 0
+
+const uint16_t PROGMEM LAYER_1_COMBO[] = {KC_ENT, KC_Y, COMBO_END};
+const uint16_t PROGMEM LAYER_2_COMBO[] = {KC_ENT, KC_U, COMBO_END};
+const uint16_t PROGMEM LAYER_3_COMBO[] = {KC_ENT, KC_I, COMBO_END};
+const uint16_t PROGMEM ESC_COMBO[] =     {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM UNDS_COMBO[] =    {KC_Y, KC_H, COMBO_END};
+const uint16_t PROGMEM EXLM_COMBO[] =    {KC_T, KC_G, COMBO_END};
+const uint16_t PROGMEM DQUO_COMBO[] =    {KC_G, KC_B, COMBO_END};
+const uint16_t PROGMEM QUOT_COMBO[] =    {KC_H, KC_N, COMBO_END};
+// copy, paste, undo, cut
+const uint16_t PROGMEM UNDO_COMBO[] =    {CTL_A, KC_Z, COMBO_END};
+const uint16_t PROGMEM CUT_COMBO[] =     {SFT_S, KC_X, COMBO_END};
+const uint16_t PROGMEM COPY_COMBO[] =    {ALT_D, KC_C, COMBO_END};
+const uint16_t PROGMEM PASTE_COMBO[] =   {GUI_F, KC_V, COMBO_END};
+
+// volume up, down, mic mute
+const uint16_t PROGMEM VOL_UP_COMBO[] =    {KC_P, CTL_SCLN, COMBO_END};
+const uint16_t PROGMEM VOL_DOWN_COMBO[] =  {CTL_SCLN, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM VOL_MUTE_COMBO[] =  {KC_P, CTL_SCLN, KC_SLSH, COMBO_END};
+
+
+combo_t key_combos[] = {
+    COMBO(LAYER_1_COMBO, TO(1)),
+    COMBO(LAYER_2_COMBO, TO(2)),
+    COMBO(LAYER_3_COMBO, TO(3)),
+	// ESC
+    COMBO(ESC_COMBO, KC_ESC),
+	// CENTER COMBOS
+	COMBO(DQUO_COMBO, KC_DQUO),
+	COMBO(QUOT_COMBO, KC_QUOT),
+	COMBO(UNDS_COMBO, KC_UNDS),
+	COMBO(EXLM_COMBO, KC_EXLM),
+
+	// COPY, PASTE, ETC
+	COMBO(UNDO_COMBO, LCTL(KC_Z)),
+	COMBO(CUT_COMBO, LCTL(KC_X)),
+	COMBO(COPY_COMBO, LCTL(KC_C)),
+	COMBO(PASTE_COMBO, LCTL(KC_V)),
+
+	// Volume up, down, mic mute
+	COMBO(VOL_UP_COMBO, KC_VOLU),
+    COMBO(VOL_DOWN_COMBO, KC_VOLD),
+    COMBO(VOL_MUTE_COMBO, KC_MUTE),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,  KC_P, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,	 CTL_A,	  SFT_S,  ALT_D,    GUI_F,  KC_G,                           KC_H,   GUI_J,   ALT_K,   SFT_L,CTL_SCLN, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            MO(2),   MO(1),  KC_TAB,   KC_ENT, KC_SPACE, KC_BSPC
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+    [1] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX, KC_ESC , KC_DQUO, KC_QUOT, KC_LPRN, KC_RPRN,                      KC_PAST,    KC_1,    KC_2,   KC_3, KC_PPLS, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, _______, _______, _______, KC_LCBR, KC_RCBR,                      KC_PSLS,    KC_4,    KC_5,    KC_6, KC_PMNS, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, _______, _______, KC_R, KC_LBRC, KC_RBRC,                     KC_EQUAL,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           _______, TO(0) , _______,    _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX,  KC_DEL, KC_EXLM,   KC_AT,  KC_HASH, KC_DLR,                       KC_TIK, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, _______, _______, _______, KC_AMPR, KC_PIPE,                       KC_TLD, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,   TO(1),   TO(2),   TO(3), KC_PERC, KC_CIRC,                      KC_UNDS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______, TO(0), _______,    _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      QK_BOOT, QK_BOOT, XXXXXXX, XXXXXXX, RGB_MOD, RGB_RMOD,                     RGB_VAI, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_TOG, RGB_TOG, XXXXXXX, XXXXXXX, RGB_HUI, RGB_HUD,                      RGB_SPI, RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAI, RGB_SAD,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          XXXXXXX, TO(0), XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
+  ),
+};
